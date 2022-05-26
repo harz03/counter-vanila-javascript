cf30c31677f4b2a54aa557cd5d508118c5f2608f
Harshit <harshit79090@gmail.com>
2022-05-26 15:43:09 +0530

    Counter app using vanilla js

---
+++ app.js
@@ -0,0 +1,20 @@
+let count = 0;
+
+const value = document.getElementById('value');
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach((items) => {
+  items.addEventListener('click', () => {
+    if (items.classList.contains('increase')) {
+      count += 1;
+      value.textContent = count;
+    } else if (items.classList.contains('decrease')) {
+      count -= 1;
+      value.textContent = count;
+    } else {
+      count = 0;
+      value.textContent = count;
+    }
+    count >= 0 ? (value.style.color = 'green') : (value.style.color = 'red');
+  });
+});
